ee9dd58c28f32a55adb2624e4d12ec2cf5afdd02
Alon Kigler <kiglerr@gmail.com>
2024-06-09 13:17:09 +0300

    app

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,18 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions/v1");
+const express = require("express");
+const cookieParser = require("cookie-parser")();
+const cors = require("cors")({ origin: true });
+const app = express();
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+app.use(cors);
+app.use(cookieParser);
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// create order by given collaction
+app.post("/", (req, res) => {
+  return res.status(200).send({ status: "Success" });
+});
+
+exports.app = functions.https.onRequest(app);
