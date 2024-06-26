4a2602a230168d604e66d7ab3af1617c43f78afc
YoyoBLemail <yoelbl2@gmail.com>
2024-06-26 21:20:31 +0300

    about

--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -1,10 +1,15 @@
 import { texts } from "../../lib/texts";
+import ImagesCarousel from "../common/imagesCarousel";
 import PageTitle from "../common/pageTitle";
 
 const About = () => {
    return (
       <section>
          <PageTitle title={texts.routesTitles.about} />
+         <div className="grid grid-cols-2 gap-4">
+            <ImagesCarousel />
+            <p>{texts.about.description}</p>
+         </div>
       </section>
    );
 };
