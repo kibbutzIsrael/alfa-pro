a7cc195eb77a9cbce03f2770e0941cbbef0919e0
Habooba <119007790+Moran-Habooba@users.noreply.github.com>
2024-07-28 11:42:59 +0300

    first commit

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,3 +1,4 @@
 export const BASE_API_URL =
-   "https://us-central1-alfa-pro-f9f22.cloudfunctions.net/app";
+  // "https://us-central1-alfa-pro-f9f22.cloudfunctions.net/app";
+  "http://127.0.0.1:5001/alfa-pro-f9f22/us-central1/app";
 export const VOLUNTEERS_API_ROUTE = "/volunteers";
