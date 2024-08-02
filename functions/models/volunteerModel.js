9334cfecca7c0084703cb236811c0561c77d8d10
YoyoBLemail <yoelbl2@gmail.com>
2024-08-02 10:30:12 +0300

    volunteersModel update: changed status enum to active and inactive

--- functions/models/volunteerModel.js
+++ functions/models/volunteerModel.js
@@ -1,49 +1,48 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const { string } = require('yup');
+const mongoose = require("mongoose");
+const validator = require("validator");
+const { string } = require("yup");
 
 const volunteerSchema = new mongoose.Schema({
-    fullName: {
-        type: String,
-        required: true,
-        minlength: 2
-    },
-    email: {
-        type: String,
-        required: true,
-        validate: [validator.isEmail, 'Not a valid Email address']
-    },
-    phoneNumber: {
-        type: String,
-        required: [true, 'No phone number'],
-        validate: {
-            validator: function(v) {
-              return /^0\d{8,9}$/.test(v);
-            },
-            message: props => `${props.value} is not a valid phone number!`
-          }
-    },
-    academicInstitution: {
-        type: String
-    },
-    whichProject: {
-        type: String
-    },
-    howYouHearAboutUs: {
-        type: String
-    },
-    status: {
-        type: String,
-        default: 'pending',
-        enum: ["pending", "approved", "declined"]
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-
+   fullName: {
+      type: String,
+      required: true,
+      minlength: 2,
+   },
+   email: {
+      type: String,
+      required: true,
+      validate: [validator.isEmail, "Not a valid Email address"],
+   },
+   phoneNumber: {
+      type: String,
+      required: [true, "No phone number"],
+      validate: {
+         validator: function (v) {
+            return /^0\d{8,9}$/.test(v);
+         },
+         message: (props) => `${props.value} is not a valid phone number!`,
+      },
+   },
+   academicInstitution: {
+      type: String,
+   },
+   whichProject: {
+      type: String,
+   },
+   howYouHearAboutUs: {
+      type: String,
+   },
+   status: {
+      type: String,
+      default: "pending",
+      enum: ["pending", "active", "inactive"],
+   },
+   createdAt: {
+      type: Date,
+      default: Date.now,
+   },
 });
 
-const Volunteer = mongoose.model('Volunteer', volunteerSchema);
+const Volunteer = mongoose.model("Volunteer", volunteerSchema);
 
 module.exports = Volunteer;
