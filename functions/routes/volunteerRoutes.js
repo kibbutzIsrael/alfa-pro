f6259d51ea683e2306f9648e65e57fd7b8d9aac5
Habooba <119007790+Moran-Habooba@users.noreply.github.com>
2024-08-04 12:34:57 +0300

    protect edit volunteer routes

--- functions/routes/volunteerRoutes.js
+++ functions/routes/volunteerRoutes.js
@@ -1,20 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const volunteerController = require('../controllers/volunteerController');
+const volunteerController = require("../controllers/volunteerController");
+const authController = require("../controllers/authController");
 
 // Route to add a new volunteer
-router.post('/', volunteerController.createVolunteer);
+router.post("/", volunteerController.createVolunteer);
 
 // Route to edit a existing volunteer
-router.put('/:id', volunteerController.updateVolunteer);
+router.put("/:id", authController.protect, volunteerController.updateVolunteer);
 
 // Route to remove a volunteer
-router.delete('/:id', volunteerController.deleteVolunteer);
+router.delete(
+  "/:id",
+  authController.protect,
+  volunteerController.deleteVolunteer
+);
 
 // Route to get all volunteers
-router.get('/' , volunteerController.getAllVolunteers);
+router.get("/", authController.protect, volunteerController.getAllVolunteers);
 
 // Route to get a volunteer by ID
-router.get('/:id' , volunteerController.getVolunteerById);
+router.get(
+  "/:id",
+  authController.protect,
+  volunteerController.getVolunteerById
+);
 
 module.exports = router;
